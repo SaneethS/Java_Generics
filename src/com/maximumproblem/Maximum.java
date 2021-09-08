5b95c75a455638580273c4f0f47e428f3f14d513
Saneeth S <saneeths@125AMBBLR-Chethan-RV.local>
2021-09-08 14:38:14 +0530

    UC1 has been created where maximum between integers has been checked

--- src/com/maximumproblem/Maximum.java
+++ src/com/maximumproblem/Maximum.java
@@ -1,6 +1,33 @@
 package com.maximumproblem;
 
 public class Maximum {
+	Integer x;
+	Integer y;
+	Integer z;
 	
+	public Maximum(Integer x, Integer y, Integer z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+	
+	public Integer maximum() {
+		return Maximum.maximum(x,y,z);
+	}
+
+	private static Integer maximum(Integer x2, Integer y2, Integer z2) {
+		Integer max = x2;
+		
+		if(y2.compareTo(max)>0)
+			max = y2;
+		if(z2.compareTo(max)>0)
+			max = z2;
+		display(x2,y2,z2,max);
+		return max;
+	}
+
+	private static void display(Integer x2, Integer y2, Integer z2, Integer max) {
+		System.out.println("Max of "+x2+", "+y2+" and "+z2+" = "+max);
+	}
 	
 }
