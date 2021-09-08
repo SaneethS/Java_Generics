2dded092875443a8d12f4773dce30dae66159576
Saneeth S <saneeths@125AMBBLR-Chethan-RV.local>
2021-09-08 14:25:08 +0530

    Welcome page is created for java generics maximum problem

---
+++ src/com/maximumproblem/Main.java
@@ -0,0 +1,9 @@
+package com.maximumproblem;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ src/com/maximumproblem/Maximum.java
@@ -0,0 +1,6 @@
+package com.maximumproblem;
+
+public class Maximum {
+	
+	
+}
